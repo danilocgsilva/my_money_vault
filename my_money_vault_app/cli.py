488f4de31b1ecace7a7a8f4cf7fd1810341b535d
Danilo Silva <contact@danilocgsilva.me>
2025-05-31 19:27:37 -0300

    wip: cli, State and Repository

--- my_money_vault_app/cli.py
+++ my_money_vault_app/cli.py
@@ -1,7 +1,21 @@
+def first_letter_upper(input_name):
+    return input_name[0].upper() + input_name[1:]
+
+def create_model_file(entity_name):
+    entity_name_model_file = f"src/Models/{first_letter_upper(entity_name)}.py"
+    with open(entity_name_model_file, "w") as file:
+        file_content = f"class {first_letter_upper(entity_name)}:"
+        file.write(file_content)
+    print(f"Model file {entity_name_model_file} created successfully!")
+
+def create_repository_file(entity_name):
+    entity_name_repository_file = f"src/Repositories/{first_letter_upper(entity_name)}Repository.py"
+    with open(entity_name_repository_file, "w") as file:
+        file_content = f"class {first_letter_upper(entity_name)}Repository:"
+        file.write(file_content)
+    print(f"Repository file {entity_name_repository_file} created successfully!")
+
 entity_name = input("Give the entity name: ")
 
-entity_name_model_file = f"src/Models/{entity_name}.py"
-
-with open(entity_name_model_file, "a") as file:
-    file_content = "class State:"
-    file.write(file_content)
+create_model_file(entity_name)
+create_repository_file(entity_name)
---
+++ my_money_vault_app/src/Models/State.py
@@ -0,0 +1,4 @@
+from typing import Optional
+
+class State:
+    id: Option
